9f726c7f693898b8f3feffcf193314abac09217c
Martin Czygan <martin.czygan@gmail.com>
2014-11-30 19:52:09 +0100

    starting public API refactorings

--- cmd/esbulk/esbulk.go
+++ cmd/esbulk/esbulk.go
@@ -15,67 +15,6 @@ import (
 	"github.com/miku/esbulk"
 )
 
-// Options represents bulk indexing options
-type Options struct {
-	Host      string
-	Port      int
-	Index     string
-	DocType   string
-	BatchSize int
-	Quiet     bool
-}
-
-// BulkIndex takes a list of documents as strings and indexes them into elasticsearch
-func BulkIndex(docs []string, options Options) error {
-	url := fmt.Sprintf("http://%s:%d/%s/%s/_bulk", options.Host, options.Port, options.Index, options.DocType)
-	header := fmt.Sprintf(`{"index": {"_index": "%s", "_type": "%s"}}`, options.Index, options.DocType)
-	var lines []string
-	for _, doc := range docs {
-		if len(strings.TrimSpace(doc)) == 0 {
-			continue
-		}
-		lines = append(lines, header)
-		lines = append(lines, doc)
-	}
-	body := fmt.Sprintf("%s\n", strings.Join(lines, "\n"))
-	response, err := http.Post(url, "application/json", strings.NewReader(body))
-	if err != nil {
-		return err
-	}
-	// > Caller should close resp.Body when done reading from it.
-	// Results in a resource leak otherwise.
-	response.Body.Close()
-	return nil
-}
-
-// Worker will batch index documents that come in on the lines channel
-func Worker(id string, options Options, lines chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	var docs []string
-	counter := 0
-	for s := range lines {
-		docs = append(docs, s)
-		counter++
-		if counter%options.BatchSize == 0 {
-			err := BulkIndex(docs, options)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if !options.Quiet {
-				fmt.Fprintf(os.Stderr, "[%s] @%d\n", id, counter)
-			}
-			docs = docs[:0]
-		}
-	}
-	err := BulkIndex(docs, options)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if !options.Quiet {
-		fmt.Fprintf(os.Stderr, "[%s] @%d\n", id, counter)
-	}
-}
-
 func main() {
 
 	version := flag.Bool("v", false, "prints current program version")
@@ -141,7 +80,7 @@ func main() {
 
 	for i := 0; i < *numWorkers; i++ {
 		wg.Add(1)
-		go Worker(fmt.Sprintf("worker-%d", i), options, queue, &wg)
+		go esbulk.Worker(fmt.Sprintf("worker-%d", i), options, queue, &wg)
 	}
 
 	// set refresh inteval to -1
--- common.go
+++ common.go
@@ -1,4 +1,74 @@
 package esbulk
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+)
+
 // Application Version
 const Version = "0.2.4"
+
+// Options represents bulk indexing options
+type Options struct {
+	Host      string
+	Port      int
+	Index     string
+	DocType   string
+	BatchSize int
+	Quiet     bool
+}
+
+// BulkIndex takes a list of documents as strings and indexes them into elasticsearch
+func BulkIndex(docs []string, options Options) error {
+	url := fmt.Sprintf("http://%s:%d/%s/%s/_bulk", options.Host, options.Port, options.Index, options.DocType)
+	header := fmt.Sprintf(`{"index": {"_index": "%s", "_type": "%s"}}`, options.Index, options.DocType)
+	var lines []string
+	for _, doc := range docs {
+		if len(strings.TrimSpace(doc)) == 0 {
+			continue
+		}
+		lines = append(lines, header)
+		lines = append(lines, doc)
+	}
+	body := fmt.Sprintf("%s\n", strings.Join(lines, "\n"))
+	response, err := http.Post(url, "application/json", strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	// > Caller should close resp.Body when done reading from it.
+	// Results in a resource leak otherwise.
+	response.Body.Close()
+	return nil
+}
+
+// Worker will batch index documents that come in on the lines channel
+func Worker(id string, options Options, lines chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var docs []string
+	counter := 0
+	for s := range lines {
+		docs = append(docs, s)
+		counter++
+		if counter%options.BatchSize == 0 {
+			err := BulkIndex(docs, options)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if !options.Quiet {
+				fmt.Fprintf(os.Stderr, "[%s] @%d\n", id, counter)
+			}
+			docs = docs[:0]
+		}
+	}
+	err := BulkIndex(docs, options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !options.Quiet {
+		fmt.Fprintf(os.Stderr, "[%s] @%d\n", id, counter)
+	}
+}
