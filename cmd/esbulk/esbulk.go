f0696db4eae3a966398d43500052767165740da9
Martin Czygan <martin.czygan@gmail.com>
2018-04-26 21:14:53 +0200

    v0.4.14

--- cmd/esbulk/esbulk.go
+++ cmd/esbulk/esbulk.go
@@ -19,7 +19,7 @@ import (
 )
 
 // Version of application.
-const Version = "0.4.13"
+const Version = "0.4.14"
 
 // indexSettingsRequest runs updates an index setting, given a body and options.
 func indexSettingsRequest(body string, options esbulk.Options) (*http.Response, error) {
--- packaging/rpm/esbulk.spec
+++ packaging/rpm/esbulk.spec
@@ -1,6 +1,6 @@
 Summary:    Fast parallel bulk loading utility for elasticsearch.
 Name:       esbulk
-Version:    0.4.13
+Version:    0.4.14
 Release:    0
 License:    MIT
 BuildArch:  x86_64
