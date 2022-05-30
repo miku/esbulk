1a26e98ce2fb424f1ec8ad03dac04fb8ab9a7a42
Martin Czygan <martin.czygan@gmail.com>
2022-05-30 10:10:45 +0200

    v0.7.7

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 TARGETS := esbulk
-VERSION := 0.7.6
+VERSION := 0.7.7
 GOLDFLAGS := "-w -s"
 
 # http://docs.travis-ci.com/user/languages/go/#Default-Test-Script
--- run.go
+++ run.go
@@ -43,7 +43,7 @@ import (
 
 var (
 	// Version of application.
-	Version = "0.7.6"
+	Version = "0.7.7"
 
 	ErrIndexNameRequired = errors.New("index name required")
 	ErrNoWorkers         = errors.New("no workers configured")
