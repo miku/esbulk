d972a240a6aa1a95de337fba9c097bb1d0463e64
Martin Czygan <martin.czygan@gmail.com>
2025-09-07 17:11:08 +0200

    v0.7.23

--- run.go
+++ run.go
@@ -43,7 +43,7 @@ import (
 
 var (
 	// Version of application.
-	Version = "0.7.20"
+	Version = "0.7.23"
 
 	ErrIndexNameRequired = errors.New("index name required")
 	ErrNoWorkers         = errors.New("no workers configured")
